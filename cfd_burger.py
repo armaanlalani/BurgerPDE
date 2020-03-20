68f8f09d4e21b14cb488bab7da86bf9932a38422
armaanlalani <60145425+armaanlalani@users.noreply.github.com>
2020-03-20 15:56:04 -0400

    Add files via upload

---
+++ cfd_burger.py
@@ -0,0 +1,51 @@
+import numpy as numpy
+import matplotlib
+matplotlib.use('Agg')
+
+def equation_of_motion(u, v):
+   un = u.copy()
+   vn = v.copy()
+
+   u[1:-1, 1:-1] = (un[1:-1, 1:-1] -
+            dt / dx * un[1:-1, 1:-1] *
+            (un[1:-1, 1:-1] - un[1:-1, 0:-2]) -
+            dt / dy * vn[1:-1, 1:-1] *
+            (un[1:-1, 1:-1] - un[0:-2, 1:-1]) +
+            nu * dt / dx**2 *
+            (un[1:-1, 2:] - 2 * un[1:-1, 1:-1] + un[1:-1, 0:-2]) +
+            nu * dt / dy**2 *
+            (un[2:, 1:-1] - 2 * un[1:-1, 1:-1] + un[0:-2, 1:-1]))
+
+   v[1:-1, 1:-1] = (vn[1:-1, 1:-1] -
+            dt / dx * un[1:-1, 1:-1] *
+            (vn[1:-1, 1:-1] - vn[1:-1, 0:-2]) -
+            dt / dy * vn[1:-1, 1:-1] *
+            (vn[1:-1, 1:-1] - vn[0:-2, 1:-1]) +
+            nu * dt / dx**2 *
+            (vn[1:-1, 2:] - 2 * vn[1:-1, 1:-1] + vn[1:-1, 0:-2]) +
+            nu * dt / dy**2 *
+            (vn[2:, 1:-1] - 2 * vn[1:-1, 1:-1] + vn[0:-2, 1:-1]))
+
+   return (u,v)
+
+def boundary(u, v, nozzle_u, nozzle_v, nx, ny, t_step):
+   u[0, :] = 0
+   u[-1, :] = 0
+   u[:, 0] = 0
+   u[:, -1] = 0
+
+   v[0, :] = 0
+   v[-1, :] = 0
+   v[:, 0] = 0
+   v[:, -1] = 0
+
+   u[ny//2-2:ny//2+2, 0] = nozzle_u[t_step]
+   v[ny//2-2:ny//2+2, 0] = nozzle_v[t_step]
+
+   return (u, v)
+
+def evolve(u, v, nozzle_u, nozzle_v, nx, ny, steps):
+   for i in range(0,steps):
+      (u, v) = equation_of_motion(u, v)
+      (u, v) = boundary(u, v, nozzle_u, nozzle_v, nx, ny , i)
+   return (u, v)
